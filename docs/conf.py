94feb81369883c17edf1e46d6dad3694a2a011f5
IrvKalb <Irv@furrypants.com>
2018-10-12 08:29:15 -0700

    Update docs/conf.py
    Changed path for finding pygwidgets

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(__file__)), 'src'))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(__file__)), 'pygwidgets'))
 
 #print()
 #for path in sys.path:
