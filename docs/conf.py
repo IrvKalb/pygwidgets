1f53e2deee568f25d8e9448d39983693d111e956
Irv Kalb <Irv@furrypants.com>
2018-12-15 12:42:13 -0800

    preparing for 0.9.4 release
    Modified configuration files for 0.9.4 release

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ author = 'Irv Kalb'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.9'
+release = '0.9.4'
 
 
 # -- General configuration ---------------------------------------------------
