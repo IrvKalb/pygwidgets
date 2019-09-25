61032118d55ad6ddda55b282d4f528248afae130
Irv Kalb <Irv@furrypants.com>
2019-09-24 21:40:39 -0700

    Update conf.py
    Updated version number to 0.9.9

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ author = 'Irv Kalb'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.9.8'
+release = '0.9.9'
 
 
 # -- General configuration ---------------------------------------------------
