399b551bec682c34f91e04ca9a1a18a4417da689
Irv Kalb <Irv@furrypants.com>
2023-07-07 09:11:44 -0700

    Updated support files.  readthedocs.yaml changed to add python version
    Updated support files.  readthedocs.yaml changed to add python version

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ author = 'Irv Kalb'
 # The short X.Y version
 version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '1.0.42'
+release = '1.0.43'
 
 
 # -- General configuration ---------------------------------------------------
